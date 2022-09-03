27388036edb6c6e0a960c8e3bfd502abc8aa50f2
Rolan Gonzalez <riolcrt@gmail.com>
2022-09-03 17:35:23 +0000

    copy shared notes automatically to destiny

--- obsidian/share-files-with-collab-doc-repo/src/main.rs
+++ obsidian/share-files-with-collab-doc-repo/src/main.rs
@@ -1,22 +1,22 @@
-use clap::Parser;
-
-use extract_frontmatter::config::Splitter;
-use extract_frontmatter::Extractor;
 use std::fs::read_dir;
 use std::fs::File;
 use std::io::Read;
 use std::path::PathBuf;
 
+use clap::Parser;
+use extract_frontmatter::config::Splitter;
+use extract_frontmatter::Extractor;
+
 #[derive(Parser)]
 struct Cli {
+    #[clap(short = 's', long = "source-dir")]
     source_dir: String,
+    #[clap(short = 'd', long = "dest-dir")]
     dest_dir: String,
+    #[clap(short = 'k', long = "key", default_value = "shared")]
     key: String,
-    branch_name: String,
-    #[clap(short, long, default_value = "Automatic sychronization")]
-    commit_message: String,
     #[clap(
-        short,
+        short = 'i',
         long,
         multiple_values = true,
         value_delimiter = ':',
@@ -25,7 +25,6 @@ struct Cli {
     ignore_folders: Vec<String>,
 }
 
-//return the content of a file as a string or a empty string if an error occurs
 fn read_file(path: &PathBuf) -> Result<String, std::io::Error> {
     let mut file = File::open(path).unwrap();
     let mut contents = String::new();
@@ -39,7 +38,6 @@ fn get_files(path: &str, key: &str, ignored_folders: &Vec<String>) -> Vec<PathBu
     for entry in read_dir(path).unwrap() {
         let entry = entry.unwrap();
         let path = entry.path();
-        let comparsion = path.is_dir() && !ignored_folders.contains(&path.display().to_string());
 
         if path.is_dir() && !ignored_folders.contains(&path.display().to_string()) {
             let path_files = get_files(path.display().to_string().as_str(), key, ignored_folders);
@@ -64,14 +62,20 @@ fn get_files(path: &str, key: &str, ignored_folders: &Vec<String>) -> Vec<PathBu
 }
 
 fn main() {
-    //parse args --source-dir <path> --dest-dir <path> --key <key> --branch <branch>
     let args: Cli = Cli::parse();
 
-    // get all the files from the source directory recursively
-    let files = get_files(&args.source_dir, &args.key, &args.ignore_folders);
+    let ignored_folders: Vec<String> = args
+        .ignore_folders
+        .iter()
+        .map(|x| args.source_dir.to_string() + "/" + x.as_str())
+        .collect();
 
-    //print the files
+    let files = get_files(&args.source_dir, &args.key, &ignored_folders);
+
     for file in files {
-        println!("{}", file.display());
+        let dest_path =
+            PathBuf::from(&args.dest_dir).join(file.strip_prefix(&args.source_dir).unwrap());
+        std::fs::create_dir_all(dest_path.parent().unwrap()).unwrap();
+        std::fs::copy(file, dest_path).unwrap();
     }
 }
